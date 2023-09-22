e07818ebedb66185ca445d3c5c941551473585b2
Gaston Soliz <gaston.e.soliz@gmail.com>
2023-09-22 14:09:04 -0300

    update

---
+++ 03-sequelize/homework-part1/01-Ejercicio/db/models/Role.js
@@ -0,0 +1,8 @@
+const { DataTypes } = require("sequelize");
+
+module.exports = (sequelize) => {
+  sequelize.define("Role", {
+    name: { type: DataTypes.STRING, unique: true, allowNull: false },
+    description: { type: DataTypes.STRING },
+  });
+};
